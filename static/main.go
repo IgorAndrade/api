14483df0baa78fbae11339728bd6d1e6dc898f0b
Igor Andrade <igor.andrade@dextra-sw.com>
2020-06-24 16:38:26 -0300

    link

--- static/main.go
+++ static/main.go
@@ -11,3 +11,6 @@ func main() {
 	log.Println("Executando...")
 	log.Fatal(http.ListenAndServe(":3000", fs))
 }
+
+//https://astaxie.gitbooks.io/build-web-application-with-golang/en/07.4.html
+//https://www.alexedwards.net/blog/serving-static-sites-with-go
